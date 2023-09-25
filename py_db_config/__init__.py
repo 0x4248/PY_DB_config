1bd6b6fe00e4a8223571082ce16c37465136f33c
lewisevans2007 <lewisevans2007@users.noreply.github.com>
2023-09-25 08:07:27 +0100

    Add function documentation.
    Signed-off-by: lewisevans2007 <lewisevans2007@users.noreply.github.com>

--- py_db_config/__init__.py
+++ py_db_config/__init__.py
@@ -15,6 +15,14 @@ class PY_DB_Config:
         self.conn.commit()
 
     def get(self, key):
+        """Get a value from the database
+
+        Args:
+            key (str): The key to get the value of
+
+        Returns:
+            str: The value of the key. Will return None if the key does not exist
+        """
         self.c.execute("SELECT value FROM config WHERE key = ?", (key,))
         result = self.c.fetchone()
         if result is None:
@@ -23,6 +31,12 @@ class PY_DB_Config:
             return result[0]
 
     def set(self, key, value):
+        """Set a value in the database
+
+        Args:
+            key (str): The key to set the value of
+            value (str): The value to set the key to
+        """   
         self.c.execute("SELECT value FROM config WHERE key = ?", (key,))
         result = self.c.fetchone()
         if result is None:
@@ -32,13 +46,28 @@ class PY_DB_Config:
         self.conn.commit()
 
     def delete(self, key):
+        """Delete a key from the database
+
+        Args:
+            key (str): The key to delete
+        """
         self.c.execute("DELETE FROM config WHERE key = ?", (key,))
         self.conn.commit()
 
     def close(self):
+        """Close the database connection
+        """
         self.conn.close()
 
     def exists(self, key):
+        """Check if a key exists in the database
+
+        Args:
+            key (str): The key to check
+
+        Returns:
+            bool: True if the key exists, False if it does not
+        """
         self.c.execute("SELECT value FROM config WHERE key = ?", (key,))
         result = self.c.fetchone()
         if result is None:
